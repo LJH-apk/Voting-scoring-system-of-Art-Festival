732bc31d586c54886a60b2ea0de130f9310e3f99
刘佳航 <18701059325@qq.com>
2021-02-01 16:18:27 +0800

    commit test

---
+++ Voting scoring system of Art Festival/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Voting scoring system of Art Festival
+//
+//  Created by 刘佳航 on 2021/2/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Voting scoring system of Art Festival/Voting_scoring_system_of_Art_FestivalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voting_scoring_system_of_Art_FestivalApp.swift
+//  Voting scoring system of Art Festival
+//
+//  Created by 刘佳航 on 2021/2/1.
+//
+
+import SwiftUI
+
+@main
+struct Voting_scoring_system_of_Art_FestivalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
